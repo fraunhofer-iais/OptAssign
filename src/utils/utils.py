01364f74ed7a45403c465b41ca86efa9dddf34c7
Maximilian Elias Fetz <maximilian.elias.fetz@iais.fraunhofer.de>
2025-02-06 00:00:13 +0100

    ci: update test workflow to use CPU-specific dependencies

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+import torch
+
 
 def safe_div(x,y):
     if y == 0:
@@ -9,3 +11,11 @@ def safe_div(x,y):
 
 def random_string(length):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+
+def cuda_else_cpu(tensor: torch.Tensor) -> torch.Tensor:
+    if torch.backends.mps.is_available():
+        return tensor.cuda()
+    if torch.backends.cuda.is_built():
+        return tensor.cuda()
+    return tensor.cpu()
